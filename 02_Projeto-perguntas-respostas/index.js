c727a8508d750f61254f5fc6d3c1cfbaa1f9c635
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-05-31 20:26:50 -0300

    Projeto 01: Perguntas e resposta - Capturando dados do formulário

--- 02_Projeto-perguntas-respostas/index.js
+++ 02_Projeto-perguntas-respostas/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+// Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -13,7 +20,10 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário eviado com sucesso!");
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send(`Formulário recebido! título: ${titulo} descrição: ${descricao}`);
 });
 
 app.listen(3000, () => {
