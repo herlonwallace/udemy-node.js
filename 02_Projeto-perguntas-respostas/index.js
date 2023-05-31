124c46bdc4c59b713fc89774b23929fddd3c8a95
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-05-31 19:01:29 -0300

    Projeto 01: Perguntas e resposta - Configuração Bootstrap

--- 02_Projeto-perguntas-respostas/index.js
+++ 02_Projeto-perguntas-respostas/index.js
@@ -4,17 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        msg: exibirMsg
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(3000, () => {
