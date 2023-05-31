dbf6d473f6c042d020fb9a6bd0ef9db21ca35308
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-05-31 18:29:36 -0300

    Projeto 01: Perguntas e resposta - Exibindo variáveis no HTML e Estruturas condicionais com EJS

--- 02_Projeto-perguntas-respostas/index.js
+++ 02_Projeto-perguntas-respostas/index.js
@@ -3,8 +3,17 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:nome/:lang", (req, res) => {
+    
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        msg: exibirMsg
+    });
 });
 
 app.listen(3000, () => {
