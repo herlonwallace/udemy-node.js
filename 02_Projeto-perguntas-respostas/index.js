8d1ea06a0ce00ca77faa23b36d6416020940d9a4
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-02 20:57:29 -0300

    Projeto 01: Perguntas e resposta - Exibição de respostas e finalização

--- 02_Projeto-perguntas-respostas/index.js
+++ 02_Projeto-perguntas-respostas/index.js
@@ -56,11 +56,17 @@ app.post("/salvarpergunta", (req, res) => {
 app.get("/pergunta/:id", (req, res) => {
     var id = req.params.id;
     Pergunta.findOne({
-        where: { id: id }
+        where: { id: id },
     }).then(pergunta => {
         if (pergunta != undefined) {
-            res.render("pergunta", {
-                pergunta: pergunta
+            Resposta.findAll({
+                where: { perguntaId: pergunta.id },
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
         } else {
             res.redirect("/");
