65382c96a8611f078364d3104c5ac38bd4305bb2
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-05 21:14:58 -0300

    Projeto 02: Blog painel admin - Salvando artigos
    Adicionando database o timezone do Brasil

--- 03_Projeto-blog-painel-admin/database/database.js
+++ 03_Projeto-blog-painel-admin/database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('blogadmin', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
+    timezone: "-03:00",
     logging: false
 })
 
