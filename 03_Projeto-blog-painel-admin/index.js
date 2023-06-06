d319a0784a9651e209f74186590db9372b3955a4
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-06 18:57:44 -0300

    Projeto 02: Blog painel admin - Menu dinâmico de categorias

--- 03_Projeto-blog-painel-admin/index.js
+++ 03_Projeto-blog-painel-admin/index.js
@@ -36,8 +36,10 @@ app.get("/", (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render("index", { articles: articles });
-    })
+        Category.findAll().then(categories => {
+            res.render("index", { articles: articles, categories: categories });
+        });
+    });
 });
 
 app.get("/:slug", (req, res) => {
@@ -48,7 +50,9 @@ app.get("/:slug", (req, res) => {
         }
     }).then(article => {
         if (article != undefined) {
-            res.render("article", { article: article });
+            Category.findAll().then(categories => {
+                res.render("article", { article: article, categories: categories });
+            });
         } else {
             res.redirect("/");
         }
