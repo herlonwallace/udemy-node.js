49dad74a4f865dc26458df5d19c9ae1fc9f8d7a8
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-07 21:29:53 -0300

    Projeto 02: Blog painel admin - Base do sistema de login

--- 03_Projeto-blog-painel-admin/index.js
+++ 03_Projeto-blog-painel-admin/index.js
@@ -5,9 +5,11 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./users/User");
 
 // View engine
 app.set('view engine', 'ejs');
@@ -28,7 +30,8 @@ connection
     });
 
 app.use("/", categoriesController);
-app.use("", articlesController);
+app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get("/", (req, res) => {
     Article.findAll({
---
+++ 03_Projeto-blog-painel-admin/users/User.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define('users', {
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//Força a criação da tabela no banco de dado, uma vez criado remove
+User.sync({ force: true })
+
+module.exports = User;
