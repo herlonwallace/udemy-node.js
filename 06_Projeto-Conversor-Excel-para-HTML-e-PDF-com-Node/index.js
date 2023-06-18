57a76cd6fc0af8f0835f60c5df2a07cc7fdc1c84
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-18 16:57:52 -0300

    Projeto 03: Conversor de Excel para HTML e PDF com Node.js - Leitura/Escrita de json

--- 06_Projeto-Conversor-Excel-para-HTML-e-PDF-com-Node/index.js
+++ 06_Projeto-Conversor-Excel-para-HTML-e-PDF-com-Node/index.js
@@ -1,9 +1,23 @@
 // fs: File-system
 const fs = require("fs");
 
-fs.writeFile("./textoParaTeste", "Nome: Wallace Herlon", (erro) => {
+fs.readFile("./usuario.json", { encoding: 'utf-8' }, (erro, dados) => { // abriu o arquivo, lendo o conteúdo.
 
     if (erro) {
-        console.log("Erro durante o salvamento...");
+        console.log("Um erro aconteceu!");
+    } else {
+
+        var conteudo = JSON.parse(dados); //converte texto para objeto javascript(JSON).
+
+        // alterando as informações que estava dentro do arquivo texto.
+        conteudo.nome = "Wallace Herlon Silva";
+        conteudo.curso = "Formação React";
+        conteudo.categoria = "React";
+
+        fs.writeFile("./usuario.json", JSON.stringify(conteudo), (erro) => { // converte objeto javascript(JSON) para texto.
+            if (erro) {
+                console.log("Um erro aconteceu durante a escrita!");
+            }
+        });
     }
-})
+});
