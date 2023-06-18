7c835b536a4a737bc6525bff7a9dad57e0ae218f
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-18 18:28:42 -0300

    Projeto 03: Conversor de Excel para HTML e PDF com Node.js - Table

--- 06_Projeto-Conversor-Excel-para-HTML-e-PDF-com-Node/app.js
+++ 06_Projeto-Conversor-Excel-para-HTML-e-PDF-com-Node/app.js
@@ -1,11 +1,15 @@
 const Processor = require("./Processor");
 var Reaader = require("./Reader");
+var Table = require("./Table");
 
 var leitor = new Reaader();
 
 async function main(){
     var dados = await leitor.Read("./users.csv");
     var dadosProcessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadosProcessados);
+    console.log(usuarios.rows);
 }
 
 main();
